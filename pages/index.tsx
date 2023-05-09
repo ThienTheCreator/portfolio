959f3e7facc071f88e4e90ecb9bc961995ec0072
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2023-05-09 00:10:09 -0700

    added some social media icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRef } from 'react';
 
 const Picture = () => (
   <Image
-    src="/pic.jpg" // Route of the image file
-    height={360} // Desired size with correct aspect ratio
-    width={360} // Desired size with correct aspect ratio
-    alt="pciture"
+    src="/pic.jpg"
+    height={360}
+    width={360}
+    alt="picture"
+  />
+);
+const LinkedInIcon = () => (
+  <Image
+    src="/linkedin.svg"
+    height={80}
+    width={80}
+    alt="LinkedInIcon"
+  />
+);
+
+const GithubLight = () => (
+  <Image
+    src="/github_light.svg"
+    height={80}
+    width={80}
+    alt="LinkedInIcon"
   />
 );
 
 export default function Home() {
+  const firstItemRef = useRef<HTMLHeadingElement>(null);
+  const secondItemRef = useRef<HTMLHeadingElement>(null);
+  const thirdItemRef = useRef<HTMLHeadingElement>(null);
   return (
     <>
       <Head>
@@ -19,36 +41,47 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-gray-400">
+      <main className="text-white">
         <nav className="flex flex-row justify-evenly text-xl">
-          <a>About</a>
-          <a>Projects</a>
-          <a>Skills</a>
-          <a>Other</a>
+          <button onClick={() => firstItemRef.current?.scrollIntoView()}>About</button>
+          <button onClick={() => secondItemRef.current?.scrollIntoView()}>Skills</button>
+          <button onClick={() => thirdItemRef.current?.scrollIntoView()}>Projects</button>
         </nav>
         <div className="centerItems">
           <div className="centerItems">
-            <h1>
+            <Picture />
+            <div className="flex fex-row">
+              <Link href="https://www.linkedin.com/in/ThienPham1">
+                <LinkedInIcon />
+              </Link>
+              <Link href="https://github.com/ThienTheCreator">
+                <GithubLight />
+              </Link>
+            </div>
+            <h1 ref={firstItemRef}>
               About Me
             </h1>
-            <Picture/>
             <p>
               Me Hello, my name is Thien. Welcome to my website!
             </p>
+            <div></div>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           </div>
           <div className="centerItems">
-            <h1>Skills</h1>
+            <h1 ref={secondItemRef}>Skills</h1>
             C++
             JavaScript/TypeScript
             HTML
             CSS
             Python
           </div>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           <div className="centerItems">
-            <h1>Projects</h1>
+            <h1 ref={thirdItemRef}>Projects</h1>
             Chess
             Music
           </div>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         </div>
       </main>
     </>
