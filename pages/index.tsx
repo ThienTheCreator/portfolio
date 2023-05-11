45693bec3a0df55dd415a8604b5173289435daff
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2023-05-10 23:52:04 -0700

    worked on the projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,8 @@ const skillsList = {
   lanuages: [
     "C++",
     "Rust",
-    "JavaScript / Typescript",
+    "Typescript",
+    "JavaScript",
     "C",
     "MySQL",
     "Java",
@@ -51,14 +52,33 @@ const skillsList = {
 }
 
 const projectsList = [
-  "2048 Clone",
-  "Chess",
-  "MineSweeper"
+  {
+    title: "2048",
+    image:
+      <Image
+        src="/project_images/2048.png"
+        height={360}
+        width={360}
+        alt="LinkedInIcon"
+      />,
+    description: "Clone of the 2048. Created a simple Ai to try to solve the game.",
+    demo: "https://thienthecreator.github.io/2048/",
+    github: "https://github.com/ThienTheCreator/2048"
+  },
+  {
+    title: "Chess",
+    image: "pic",
+    description: "blah"
+  },
+  {
+    title: "MineSweeper",
+    image: "pic",
+    description: "blah"
+  },
 ]
 
 export default function Home() {
   const topRef = useRef<HTMLHeadingElement>(null);
-  const aboutRef = useRef<HTMLHeadingElement>(null);
   const skillsRef = useRef<HTMLHeadingElement>(null);
   const projectsRef = useRef<HTMLHeadingElement>(null);
   return (
@@ -72,12 +92,11 @@ export default function Home() {
       <main>
         <h1 ref={topRef}></h1>
         <nav className="flex flex-row justify-evenly text-2xl my-4">
-          <button onClick={() => aboutRef.current?.scrollIntoView({ behavior: "smooth" })}>About</button>
           <button onClick={() => skillsRef.current?.scrollIntoView({ behavior: "smooth" })}>Skills</button>
           <button onClick={() => projectsRef.current?.scrollIntoView({ behavior: "smooth" })}>Projects</button>
         </nav>
         <div className="centerItems">
-          <div className="centerItems">
+          <div className="centerItems my-48">
             <Picture />
             <div className="flex fex-row">
               <Link href="https://www.linkedin.com/in/ThienPham1">
@@ -87,31 +106,53 @@ export default function Home() {
                 <GithubLight />
               </Link>
             </div>
-            <h1 ref={aboutRef} className="my-2">
+            <h1 className="my-2">
               About Me
             </h1>
             <p>
-              Me Hello, my name is Thien.
+              Hello, my name is Thien. I am a developer.
             </p>
           </div>
-          <div className="centerItems">
+          <div className="centerItems mb-96">
             <h1 ref={skillsRef} className="my-2">Skills</h1>
             <button onClick={() => topRef.current?.scrollIntoView({ behavior: "smooth" })}>↑To Top</button>
-            <h2>Lanuages</h2>
+            <h2 className="my-12">Lanuages</h2>
             <ul>
               {skillsList.lanuages.map((item) => <li>{item}</li>)}
             </ul>
-            <h2>Tools & Frameworks</h2>
+            <h2 className="my-12">Tools & Frameworks</h2>
             <ul>
               {skillsList.toolsFrameworks.map((item) => <li>{item}</li>)}
             </ul>
           </div>
-          <div className="centerItems my-1">
-            <h1 ref={projectsRef}>Projects</h1>
+          <div className="centerItems">
+            <h1 ref={projectsRef} className="my-2">Projects</h1>
             <button className="ml-4" onClick={() => topRef.current?.scrollIntoView({ behavior: "smooth", block: "center" })}>↑To Top</button>
-            <ul>
-              {projectsList.map((item) => <li>{item}</li>)}
-            </ul>
+            {projectsList.map((item) =>
+              <div className="border rounded-2xl w-[60rem] my-4">
+                <h2 className="centerItems m-2">{item.title}</h2>
+                <div className="flex flex-row">
+                  <div className="m-5">
+                    {item.image}
+                  </div>
+                  <div className="my-auto">
+                    <p>
+                      {item.description}
+                    </p>
+                    <div className="flex justify-evenly">
+                      {item.demo !== undefined
+                        ? <a href={item.demo} target="_blank" rel="noopener noreferrer" className="centerItems w-[100px] bg-neutral-500">Demo</a>
+                        : null
+                      }
+                      {item.demo !== undefined
+                        ? <a href={item.github} target="_blank" rel="noopener noreferrer" className="centerItems w-[100px] bg-neutral-500">GitHub</a>
+                        : null
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </main>
