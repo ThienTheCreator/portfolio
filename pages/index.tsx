4f0b2dfc471e62bac6a61489f871345d5859a614
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2023-06-19 16:56:32 -0700

    change font styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,10 +53,11 @@ export default function Home() {
 				<meta name="description" content="Thien Pham Portfolio" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
+				<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&display=swap" rel="stylesheet" />
 			</Head>
-			<main>
+			<div className="w-full">
 				<h1 ref={topRef}></h1>
-				<nav className="flex flex-row justify-evenly text-2xl py-4 bg-black border border-[#808080] border-x-transparent border-t-transparent">
+				<nav className="flex flex-row justify-evenly text-2xl py-4 bg-black border border-[#808080] border-x-transparent border-t-transparent font-orbitron">
 					<button onClick={() => skillsRef.current?.scrollIntoView({ behavior: "smooth" })}>Skills</button>
 					<button onClick={() => projectsRef.current?.scrollIntoView({ behavior: "smooth" })}>Projects</button>
 				</nav>
@@ -74,12 +75,12 @@ export default function Home() {
 									<GithubLight />
 								</Link>
 							</div>
-							<h1 className="flex flex-wrap justify-center my-4">
+							<h1 className="flex flex-wrap justify-center my-4 font-orbitron">
 								About Me
 							</h1>
-							<div className="flex flex-wrap justify-center my-4">
+							<div className="flex flex-wrap justify-center my-4 w-[300px]">
 								<p>
-									Hello, my name is Thien. I am a developer.
+									Hello, my name is Thien. I am a developer. I started programing to create better software to improve problems in my life. I hope that what I create has been useful for you or others.
 								</p>
 							</div>
 						</div>
@@ -87,10 +88,10 @@ export default function Home() {
 					<div className="flex flex-wrap justify-center">
 						<div className="flex flex-col mb-4 h-[60rem]">
 							<div className="flex flex-wrap justify-center">
-								<h1 ref={skillsRef} className="my-4">Skills</h1>
+								<h1 ref={skillsRef} className="my-4 font-orbitron">Skills</h1>
 								<button className="ml-4" onClick={() => topRef.current?.scrollIntoView({ behavior: "smooth" })}>↑To Top</button>
 							</div>
-							<div className="grow flex flex-col justify-center ">
+							<div className="flex flex-col justify-center ">
 								<div className="flex flex-wrap justify-center">
 									<h2 className="my-12">Lanuages</h2>
 								</div>
@@ -111,32 +112,34 @@ export default function Home() {
 						</div>
 					</div>
 					<div className="flex justify-center">
-						<div>
+						<div className="w-[720px]">
 							<div className="flex flex-wrap justify-center">
-								<h1 ref={projectsRef} className="my-2">Projects</h1>
+								<h1 ref={projectsRef} className="my-2 font-orbitron">Projects</h1>
 								<button className="ml-4" onClick={() => topRef.current?.scrollIntoView({ behavior: "smooth", block: "center" })}>↑To Top</button>
 							</div>
 							{projectsList.map((item) =>
-								<div key={item.key} className="flex flex-col min-h-[720px] border-hidden border-8 rounded-2xl my-16 bg-neutral-700">
+								<div key={item.key} className="flex flex-col min-h-[720px] border-hidden border-8 rounded-2xl my-20 bg-neutral-700">
 									<div className="flex flex-wrap justify-center">
-										<h2 className="m-2">{item.title}</h2>
+										<div className="m-2 font-orbitron text-6xl">{item.title}</div>
 									</div>
-									<div className="flex flex-1">
-										<div className="flex flex-wrap flex-row justify-center">
-											<div className="flex justify-center m-auto w-[300px]">
+									<div className="flex flex-1 justify-center">
+										<div className="flex flex-wrap justify-center">
+											<div className="flex justify-center p-[10px] w-[360px]">
 												{item.image}
 											</div>
-											<div className="my-auto w-80 inline-block">
-												<p>
-													{item.description}
-												</p>
-												<div className="flex justify-evenly w-full my-5">
+											<div className="my-auto w-[300px]">
+												<div className="flex justify-center">
+													<p className="text-justify">
+														{item.description}
+													</p>
+												</div>
+												<div className="flex flex-1 justify-center space-x-[10px] w-[300px] my-5">
 													{item.demo !== undefined
-														? <a href={item.demo} target="_blank" rel="noopener noreferrer" className="grid place-content-center w-36 h-16 bg-neutral-500">Demo</a>
+														? <a href={item.demo} target="_blank" rel="noopener noreferrer" className="grid place-content-center w-28 h-16 bg-neutral-500">Demo</a>
 														: null
 													}
 													{item.demo !== undefined
-														? <a href={item.github} target="_blank" rel="noopener noreferrer" className="grid place-content-center w-36 h-16 bg-neutral-500">GitHub</a>
+														? <a href={item.github} target="_blank" rel="noopener noreferrer" className="grid place-content-center w-28 h-16 bg-neutral-500">GitHub</a>
 														: null
 													}
 												</div>
@@ -148,7 +151,7 @@ export default function Home() {
 						</div>
 					</div>
 				</div>
-			</main>
+			</div>
 			<footer>
 				<div className="flex flex-wrap justify-center mb-4">
 					2023 © Thien Pham
